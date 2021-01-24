5b4a28fafb4eb24c9c20d1e34921172a9a723cef
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-01-24 09:57:17 +0530

    images from keras dataset

---
+++ classifier1.py
@@ -0,0 +1,14 @@
+import ssl
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images,train_labels),(test_images,test_labels) = fashion_mnist.load_data()
+
+print(test_labels[0])
+plt.imshow(test_images[8],cmap='gray',vmin=0,vmax=255)
+plt.show()
